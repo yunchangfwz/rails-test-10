a345feb6963f185523b2851712bf69ff9d93bea1
yunchang <yunchang@futureworkz.com>
2016-07-02 11:38:57 +0700

    Add devise#user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "==== Create User ===="
+User.destroy_all
+  ActiveRecord::Base.connection.execute(
+    "ALTER SEQUENCE users_id_seq RESTART WITH 1"
+  )
+User.create!([ 
+  { email: 'yunchang@example.com',
+    password: '123123123',
+    password_confirmation:  '123123123'
+  },
+  { email: 'theo@example.com',
+    password: '123123123',
+    password_confirmation:  '123123123'
+  },
+  { email: 'smith@example.com',
+    password: '123123123',
+    password_confirmation:  '123123123'
+  }
+    ])
+
+puts "==== End ===="
